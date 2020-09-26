53d211757e57691551b7bc00c54bb9fed2360ce0
Joshua Martin <the.manuscriptmaster@gmail.com>
2020-09-25 20:22:10 -0400

    Stretch goal

--- src/util/index.js
+++ src/util/index.js
@@ -13,6 +13,17 @@ const {
   where,
 } = R;
 
+/**
+ * Todo:
+ * - Auto-stores files in year/month folders.
+ * - Stretch goal: how can I separate the date an entry was written
+ *   from the definition of how to store and name it?
+ *   E.g. treating journal organization as a user property:
+ *   - Given a date of August 1, 2020
+ *   - And a user-defined folder organization ['YYYY', 'M', 'YYYY-MM-DD'] with a sane default
+ *   - Setup a file in path 2020 > August > 2020-08-01
+ */
+
 export const mapAsync = curryN(2, pipe(map, arr => Promise.all(arr)));
 
 export const pathString = curry((string, object) => path(split('.', string), object));
