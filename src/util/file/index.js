2432c3628713c23a53032aa0c8c0e2e6cc00ff0c
Joshua Martin <the.manuscriptmaster@gmail.com>
2020-10-02 08:09:44 -0400

    More todos and ideas

--- src/util/file/index.js
+++ src/util/file/index.js
@@ -8,20 +8,16 @@ const {
 } = R;
 
 /**
- * @todo
- * - Replace all refs to 'process.cwd()/entries/{filename} with function call.
- * - Auto-stores files in year/month folders.
- * - Stretch goal: how can I separate the date an entry was written
- *   from the definition of how to store and name it?
- *   E.g. treating journal organization as a user property:
- *   - Given a date of August 1, 2020
- *   - And a user-defined env variable such as PATTERN="~/Desktop/My Coffee Journal/{YY}/{MM}/{filename}"
- *   - Setup a file in path User > Desktop > My Coffee Journal > 2020 > August > 2020-08-01
- *   - Update VSCode settings to target this path with JSON Schema validation
- * - Create migration from one folder structure to the other (because I need this!).
- *   - "pourover journal migrate --from --to" or have from/to as positional arguments
- *   - Updates config after successful migration
- * - Dynamically generate JSON Schema to lift out complex patterns and other behavior. Libraries?
+ * @todo Filepath is passed in by user but has a sane default in codebase.
+ * - Given a date of August 1, 2020
+ * - And a user-defined env variable such as PATTERN="~/Desktop/My Coffee Journal/{YY}/{MM}/{filename}"
+ * - Setup a file in path User > Desktop > My Coffee Journal > 2020 > August > 2020-08-01
+ * - Update VSCode settings to target this path with JSON Schema validation
+ * @todo Create migration from one folder structure to the other (because I need this!).
+ * - "pourover journal migrate --from --to" or have from/to as positional arguments
+ * - Updates config after successful migration
+ * @todo Create script to update all entries based on a schema change (e.g. adding keywords, which defaults to an empty array)
+ * @todo Dynamically generate JSON Schema to lift out complex patterns and other behavior. Libraries?
  */
 
 const __filename = fileURLToPath(import.meta.url);
