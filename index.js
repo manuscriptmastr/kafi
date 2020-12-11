899e844e4135df457eff56bed8791ca28046f947
Joshua Martin <the.manuscriptmaster@gmail.com>
2020-12-10 21:35:52 -0500

    Todos

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 import commands from './src/cli';
 
+/**
+ * @todo Change pourover => coffee-cli (or something catchy!) and pourover-cli to said catchy name
+ * @todo coffee-cli [pourover|cupping] journal
+ * @todo coffee-cli [pourover|cupping] stats
+ * @todo coffee-cli [pourover|cupping] update
+ */
+
 yargs(hideBin(process.argv))
   .command(commands)
   .demandCommand()
